519b3c9dac71a7d72b40b3e2afa8daf4f2afb473
rglepe <rglepe@gmail.com>
2022-03-01 02:10:45 +0100

    server branch

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ import app from './app.js';
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3000;
-app.listen(3000, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
