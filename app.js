33bbba205741189ae17bf069a40b69de4a0d86b7
rglepe <rglepe@gmail.com>
2022-03-01 10:25:52 +0100

    minor fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import imageRouter from './routes/imageRouter.js';
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/api/image', imageRouter);
